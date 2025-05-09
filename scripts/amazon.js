6c0d3bbaa730e6b48c1fdea2df9ee85ed101a248
cxnfvs3d <cxnfused3@gmail.com>
2025-05-09 23:47:11 +0300

    cart quantity update

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -71,11 +71,18 @@ document.querySelectorAll('.js-add-to-cart').forEach((btn)=>{
       matchingItem.quantity ++;
     }else{
       cart.push({
-        productId,
+        productId: productId,
         quantity: 1
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
+
