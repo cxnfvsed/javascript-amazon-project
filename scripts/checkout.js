32862de5cb052cedc34367a1bf32580f38d17760
cxnfvs3d <cxnfused3@gmail.com>
2025-05-16 00:22:05 +0300

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderPaymentSummary } from '../checkout/paymentSummary.js';
 import { renderOrderSummary } from '../checkout/orderSummary.js';
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
