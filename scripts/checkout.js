82a18223dd88c2700c98741b465650f063828312
cxnfvs3d <cxnfused3@gmail.com>
2025-05-17 18:06:25 +0300

    convert products from xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,17 @@
 import { renderPaymentSummary } from '../checkout/paymentSummary.js';
 import { renderOrderSummary } from '../checkout/orderSummary.js';
-import { loadProducts } from '../data/products.js';
+import { loadProductsFetch } from '../data/products.js';
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 
+loadProductsFetch().then((values)=>{
+  renderOrderSummary();
+  renderPaymentSummary();
+})
+
+/*
 loadProducts(()=>{
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
